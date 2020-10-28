7cf79aa35ec9545c0d0a384879ab3d520bfe61a0
lane froehlich <elaine.froehlich@gmail.com>
2020-10-28 11:29:45 -0400

    Using class and instance variables
    Using class and instance variables to have different values for pay raises

--- employee_class_variables.py
+++ employee_class_variables.py
@@ -4,6 +4,7 @@
 class Employee:
     # class variables should be the same for all
     raise_amount = 1.04
+    num_of_emps = 0
 
     #  this is like the constructor
     def __init__(self, first, last, pay):
@@ -12,6 +13,8 @@ class Employee:
         self.last = last
         self.pay = pay
         self.email = first + "." + last + "@company.com"
+
+        Employee.num_of_emps += 1
 
     def fullname(self):
         #  using self.raise_amount allows us to change the raise_amount for a single instance
@@ -22,13 +25,18 @@ class Employee:
         self.pay = int(self.pay * self.raise_amount)
 
 
+print(Employee.num_of_emps)
+
 #  creating instances of the employee class
 #  self passed in automatically
 emp_1 = Employee("Corey", "Schafer", 75000)
 emp_2 = Employee("test", "user", 85000)
 
 
-print(Employee.raise_amount)
+print(Employee.num_of_emps)
+
+
+# print(Employee.raise_amount)
 
 #  prints {'pay': 75000, 'last': 'Schafer', 'email': 'Corey.Schafer@company.com', 'first': 'Corey'}  no raise_amount in the dict
 # print(emp_1.__dict__)
@@ -44,8 +52,8 @@ emp_1.raise_amount = 1.08
 # print(Employee.__dict__)
 
 #  instance accesses the classes attribute
-print(emp_1.raise_amount)
-print(emp_2.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
 
 
 # print(emp_1)
